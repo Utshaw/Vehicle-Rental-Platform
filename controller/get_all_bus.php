dc95ab1aa11efc808c6bbfbfe32474fe74a861b8
k1605986@kingston.ac.uk <farhan.tanvir.utshaw@gmail.com>
2019-05-13 00:09:38 +0600

    Add controller for all bus

---
+++ controller/get_all_bus.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: utshaw
+ * Date: 5/12/19
+ * Time: 11:59 PM
+ */
+
+
+require_once '../model/dbconnection.php';
+require_once '../model/bus.php';
+require_once '../model/dataAccess.php';
+
+$daoObject = DAO::getInstance();
+
+$results = [];
+
+$bus = new Bus();
+$results = $daoObject->getAllBuses();
+
