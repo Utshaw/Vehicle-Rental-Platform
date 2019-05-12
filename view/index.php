50e69fa93dbeb1ad279b6970dc8a4628d9e172d8
k1605986@kingston.ac.uk <farhan.tanvir.utshaw@gmail.com>
2019-05-13 00:09:51 +0600

    Add index

--- view/index.php
+++ view/index.php
@@ -1,8 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: utshaw
- * Date: 5/12/19
- * Time: 11:54 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vehicle Rental Platform</title>
+</head>
+<body>
 
+<?php  require_once "../controller/get_all_bus.php"?>
+
+<?php foreach ($results
+
+as $bus):
+?>
+
+<p><?=$bus->VEHICLE_ID . ' ' . $bus->MAKE_NAME . ' ' . $bus->MODEL_NAME  ?></p>
+
+<?php endforeach; ?>
+
+
+</body>
+</html>
