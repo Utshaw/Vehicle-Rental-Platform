c10b7f9a11f7c88ae0cc79b85815581545713244
k1605986@kingston.ac.uk <farhan.tanvir.utshaw@gmail.com>
2019-05-13 00:01:12 +0600

    Add bus

---
+++ model/bus.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+class Bus {
+
+    private $VEHICLE_ID;
+    private $MAKE_NAME;
+    private $MODEL_ID;
+    private $COLOUR;
+    private $DAILY_RATE;
+    private $TOTAL_NUMBER_OF_VEHICLES;
+    private $IMAGE;
+    private $MODEL_TYPE;
+    private $MAX_CAPACITY;
+    private $MIN_COST;
+    private $MAX_COST;
+    private $DATE_REQUIRED;
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+}
