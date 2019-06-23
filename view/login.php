7985fe1c20d20fb8a87d79940f1fff661462af46
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-06-23 21:53:10 +0600

    Add login system

--- view/login.php
+++ view/login.php
@@ -31,7 +31,7 @@ require_once "../controller/login_controller.php";
   <link rel="stylesheet" href="../css/style.css">
 
 
-  <title>BERWYN LTD.</title>
+  <title>MUS HIRE LTD.</title>
 </head>
 
 
@@ -46,8 +46,8 @@ require_once "../controller/login_controller.php";
 
 <form class="form-signin" action="" method="post">
 
-    <img class="mb-4" src="../images/favicon.png" alt="Company Logo" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Berwyn Ltd</h1>
+    <img class="mb-4" src="../images/favicon.png" alt="Company Logo" width="200">
+    <h1 class="h3 mb-3 font-weight-normal">MUS HIRE LTD</h1>
     <label for="inputEmail" class="sr-only">Email address</label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email">
     <label for="inputPassword" class="sr-only">Password</label>
