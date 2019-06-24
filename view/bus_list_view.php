d0df93891a9358c5dc596327746fe5224fae3e17
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-06-24 06:44:09 +0600

    Update bus list view and remove requirement of license

--- view/bus_list_view.php
+++ view/bus_list_view.php
@@ -33,7 +33,7 @@
                 <img class="card-img-top" src="../images/<?= $bus->IMAGE ?>  " alt="Image of the bus" height="150">
                 <div class="card-body">
                     <h5 class="card-title"><?= $bus->MAKE_NAME ?></h5>
-                    <p class="card-text">DAILY RATE: &#163; <?= $bus->DAILY_RATE ?></p>
+                    <p class="card-text">DAILY RATE: &#2547; <?= $bus->DAILY_RATE ?></p>
                     <p>CAPACITY: <?=$bus->MAX_CAPACITY?></p>
                     <p><button class="cartitem btn btn-primary" data-reqdate="<?=$date?>" data-id="<?=$bus->VEHICLE_ID?>" data-make="<?=$bus->MAKE_NAME?>" data-model="<?= $bus->MODEL_NAME ?>" data-price="<?= $bus->DAILY_RATE ?>">Add to cart</button></p>
                 </div>
