5633c749038888c1d3536926197f34db15655a0e
MasfiqurRahaman <masfiqallid@gmail.com>
2019-07-22 14:30:08 +0600

    review messeage bug fixed

--- view/test2.php
+++ view/test2.php
@@ -7,49 +7,19 @@
     <title>Document</title>
 
     <style>
-        body { font-size: 18px; }
-
-        .stars-container {
-        position: relative;
-        display: inline-block;
-        color: transparent;
-        }
-
-        .stars-container:before {
-        position: absolute;
-        top: 0;
-        left: 0;
-        content: '★★★★★';
-        color: lightgray;
-        }
-
-        .stars-container:after {
-        position: absolute;
-        top: 0;
-        left: 0;
-        content: '★★★★★';
-        color: gold;
-        overflow: hidden;
-        }
-
-        .stars-0:after { width: 0%; }
-        .stars-10:after { width: 10%; }
-        .stars-20:after { width: 20%; }
-        .stars-30:after { width: 30%; }
-        .stars-40:after { width: 40%; }
-        .stars-50:after { width: 50%; }
-        .stars-60:after { width: 60%; }
-        .stars-70:after { width: 70%; }
-        .stars-80:after { width: 80%; }
-        .stars-90:after { width: 90%; }
-        .stars-100:after { width: 100; }
+       
     </style>
 
 
 </head>
 <body>
-    <?php $val = 50?>
-    <div><span class="stars-container stars-<?=$val?>">★★★★★</span></div>
+    <script>
+        const date1 = new Date('2019-07-22');
+        const date2 = new Date('2019-07-20');
+        const diffTime = Math.abs(date2.getTime() - date1.getTime());
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+        alert(diffDays);
+        </script>
 </body>
 
 
