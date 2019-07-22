2fd69664f1bdb766e3c29df9c9350b6948620b23
MasfiqurRahaman <masfiqallid@gmail.com>
2019-07-22 12:51:05 +0600

    rating showing for each bus.

--- view/bus_list_view.php
+++ view/bus_list_view.php
@@ -1,5 +1,51 @@
+<style>
+    .stars-container {
+        position: relative;
+        display: inline-block;
+        color: transparent;
+    }
 
+    .stars-container:before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '★★★★★';
+        color: lightgray;
+    }
 
+    .stars-container:after {
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '★★★★★';
+        color: gold;
+        overflow: hidden;
+    }
+
+    .stars-0:after {
+        width: 0%;
+    }
+
+    .stars-1:after {
+        width: 20%;
+    }
+
+    .stars-2:after {
+        width: 40%;
+    }
+
+    .stars-3:after {
+        width: 60%;
+    }
+
+    .stars-4:after {
+        width: 80%;
+    }
+
+    .stars-5:after {
+        width: 100%;
+    }
+</style>
 
 
 <div class="content">
@@ -11,72 +57,99 @@
 
         <?php foreach ($results
 
-                       as $bus):
+            as $bus) :
+
             ?>
+
+
 
             <?php
 
-            if ($num_buses % 4 == 0):
+            if ($num_buses % 4 == 0) :
 
 
                 ?>
                 <div class="card-group">
 
 
-            <?php
-            endif ;
-            $num_buses = $num_buses + 1; ?>
+                <?php
+                endif;
+                $num_buses = $num_buses + 1; ?>
 
 
 
 
 
-            <div class="card card text-white bg-dark mb-3">
-                <img class="card-img-top" src="../images/<?= $bus->IMAGE ?>  " alt="Image of the bus" height="150">
-                <div class="card-body">
-                    <a target="_blank" href="http://localhost/VRP/view/bus.php?vehicle_id=<?=$bus->VEHICLE_ID?>"><h5 class="card-title"><?= $bus->MAKE_NAME ?></h5></a>
-                    <p class="card-text">DAILY RATE: &#2547; <?= $bus->DAILY_RATE ?></p>
-                    <p>CAPACITY: <?=$bus->MAX_CAPACITY?></p>
 
-                    <p>COMPANY: <?=$bus->COMPANY_NAME?></p>
-                    <p><button class="cartitem btn btn-primary" data-date_from="<?=$date_from?>" data-date_to="<?=$date_to?>"  data-booking_date="<?=$booking_date?>" data-id="<?=$bus->VEHICLE_ID?>" data-make="<?=$bus->MAKE_NAME?>" data-model="<?= $bus->MODEL_NAME ?>" data-price="<?= $bus->DAILY_RATE ?>">Add to cart</button></p>
-                </div>
-                <div class="card-footer">
-                    <small><?=$bus->COMPANY_NAME?></small> <br>
-                    <small style="color: bisque"><?= $bus->MODEL_NAME ?></small>
-                </div>
-            </div>
-
-            <?php
-            if ($num_buses % 4 == 0){
-
-
-                ?>
-                </div>
-
-            <?php } if($num_buses == $total_result) : ?>
-
-            <?php while( ($num_buses % 4 )!= 0 ) : ?>
-
-                <div class="card card text-white bg-dark mb-3" style="visibility: hidden;">
+                <div class="card card text-white bg-dark mb-3">
+                    
                     <img class="card-img-top" src="../images/<?= $bus->IMAGE ?>  " alt="Image of the bus" height="150">
+                    
                     <div class="card-body">
-                        <h5 class="card-title"><?= $bus->MAKE ?></h5>
-                        <p class="card-text">DAILY RATE:<?= $bus->DAILY_RATE ?></p>
-                    </div>
+                        <a target="_blank" href="http://localhost/VRP/view/bus.php?vehicle_id=<?= $bus->VEHICLE_ID ?>">
+                            <h5 class="card-title"><?= $bus->MAKE_NAME ?></h5>
+                        </a>
 
-                    <div class="card-footer">
-                        <span style="color: red"><?= $bus->MODEL_NAME ?></span>
+                        <!-- <p><span  class="stars-container stars-<?= ceil($bus->RATING) ?>">★★★★★</span></p> -->
+
+                        <p>Vehicle Rating: <? if(!isset($bus->RATING)){ echo "No rating found";} else {echo $bus->RATING;}?></p>
+
+
+                        <!-- <span class="stars-container stars-50">★★★★★</span> -->
+
+
+
+
+
+                        <p class="card-text">DAILY RATE: &#2547; <?= $bus->DAILY_RATE ?></p>
+                        <p>CAPACITY: <?= $bus->MAX_CAPACITY ?></p>
+
+                        <p>COMPANY: <?= $bus->COMPANY_NAME ?></p>
+
+
+
+                        <p><button class="cartitem btn btn-primary" data-date_from="<?= $date_from ?>" data-date_to="<?= $date_to ?>" data-booking_date="<?= $booking_date ?>" data-id="<?= $bus->VEHICLE_ID ?>" data-make="<?= $bus->MAKE_NAME ?>" data-model="<?= $bus->MODEL_NAME ?>" data-price="<?= $bus->DAILY_RATE ?>">Add to cart</button></p>
                     </div>
+                    <div class="card-footer">
+                        <small><?= $bus->COMPANY_NAME ?></small> <br>
+
+                        <small style="color: bisque"><?= $bus->MODEL_NAME ?></small>
+                    </div>
+                    
                 </div>
+
 
                 <?php
-                $num_buses++;
-            endwhile;
+                if ($num_buses % 4 == 0) {
+
+
+                    ?>
+                </div>
+
+            <?php }
+            if ($num_buses == $total_result) : ?>
+
+                <?php while (($num_buses % 4) != 0) : ?>
+
+                    <div class="card card text-white bg-dark mb-3" style="visibility: hidden;">
+                        <img class="card-img-top" src="../images/<?= $bus->IMAGE ?>  " alt="Image of the bus" height="150">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $bus->MAKE ?></h5>
+                            <p class="card-text">DAILY RATE:<?= $bus->DAILY_RATE ?></p>
+                        </div>
+
+                        <div class="card-footer">
+                            <span style="color: red"><?= $bus->MODEL_NAME ?></span>
+                        </div>
+                    </div>
+
+                    <?php
+                    $num_buses++;
+                endwhile;
             endif;
             ?>
 
-            <?php endforeach; ?>
+        <?php endforeach; ?>
 
 
 
--- view/test2.php
+++ view/test2.php
@@ -1,16 +1,60 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-class Test {
+    <style>
+        body { font-size: 18px; }
 
-    public function foo() {
-        echo "this is some";
-    }
+        .stars-container {
+        position: relative;
+        display: inline-block;
+        color: transparent;
+        }
 
-    public function foo2() {
-        echo " this is farhan";
-        $this->foo();
-    }
-}
+        .stars-container:before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '★★★★★';
+        color: lightgray;
+        }
 
-$test = new Test();
-$test->foo2();
+        .stars-container:after {
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '★★★★★';
+        color: gold;
+        overflow: hidden;
+        }
+
+        .stars-0:after { width: 0%; }
+        .stars-10:after { width: 10%; }
+        .stars-20:after { width: 20%; }
+        .stars-30:after { width: 30%; }
+        .stars-40:after { width: 40%; }
+        .stars-50:after { width: 50%; }
+        .stars-60:after { width: 60%; }
+        .stars-70:after { width: 70%; }
+        .stars-80:after { width: 80%; }
+        .stars-90:after { width: 90%; }
+        .stars-100:after { width: 100; }
+    </style>
+
+
+</head>
+<body>
+    <?php $val = 50?>
+    <div><span class="stars-container stars-<?=$val?>">★★★★★</span></div>
+</body>
+
+
+
+
+
+</html>
+
